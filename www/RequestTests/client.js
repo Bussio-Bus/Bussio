867925ced39c75cbc70fc5255ec3bd45619b6b4f
stefan-epic <ha.stefan02@gmail.com>
2022-01-28 22:20:09 +0100

    Added some test code to client.js

--- www/RequestTests/client.js
+++ www/RequestTests/client.js
@@ -2,7 +2,14 @@ import fetch from "node-fetch"
 
 
 //Gets information from URL and returns Promise which has to be resolved using then
-fetch('https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=Brixen%Dantestraße&mode=direct&outputFormat=json&itdDateDayMonthYear=29-01-2022')
-    .then(response => response.json())
-    .then(data => console.log(data));
+let json = null;
+await fetch('https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=Brixen%Dantestraße&mode=direct&outputFormat=json&itdDateDayMonthYear=29-01-2022')
+    .then(response => response.json()) //the fetch returns the entire HTTP response so inorder to extract it the .json() Method has to be used which returns another promise
+    .then(data => json = data); //data is the actual information we need in json
+
+console.log(json.servingLines.lines)
+
+for (const linesKey in json.servingLines.lines) {
+    console.log(json.servingLines.lines[linesKey].mode.destination + " " + json.servingLines.lines[linesKey].mode.name)
+}
 
